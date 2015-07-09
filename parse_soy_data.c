bf1d5efc76475698a81e7556c61feeb4441e0259
Scott Rohde <srohde@igb.illinois.edu>
2015-07-09 15:10:52 -0500

    Refactored code to eliminate long, repetitive if-else if-else block.
    Tweak comments.

--- parse_soy_data.c
+++ parse_soy_data.c
@@ -4,17 +4,39 @@
 #include <libxml/xmlmemory.h>
 #include <libxml/parser.h>
 
+int
+recognized_variable(xmlChar *variable_name) {
+    if (xmlStrEqual(variable_name,  (const xmlChar *) "solar") 
+        || xmlStrEqual(variable_name,  (const xmlChar *) "temp")
+        || xmlStrEqual(variable_name,  (const xmlChar *) "rh")
+        || xmlStrEqual(variable_name,  (const xmlChar *) "wind")
+        || xmlStrEqual(variable_name,  (const xmlChar *) "precipitation")
+        )
+        {
+            return 1;
+        }
+    else {
+        return 0;
+    }
+}
+
 /** Given an xmlDocPtr "doc" and an xmlNotePtr "cur" that points to an
  * "observation" element, print out the information contained in that
  * observation element. */
 void
 display_observation(xmlDocPtr doc, xmlNodePtr cur) {
 
-    xmlChar *year, *day, *hour, *solar, *temp, *units, *rh, *wind, *precipitation;
+    xmlChar* year;
+    xmlChar* day;
+    xmlChar* hour;
+    
+    xmlChar *variable_name;
+    xmlChar *variable_value;
+    xmlChar *units;
 
-    year = xmlGetProp(cur, "year");
-    day = xmlGetProp(cur, "day-of-year");
-    hour = xmlGetProp(cur, "hour");
+    year = xmlGetProp(cur, (const xmlChar *) "year");
+    day = xmlGetProp(cur, (const xmlChar *) "day-of-year");
+    hour = xmlGetProp(cur, (const xmlChar *) "hour");
 
     printf("Observation at hour %s of day %s of %s:\n", hour, day, year);
 
@@ -27,46 +49,30 @@ display_observation(xmlDocPtr doc, xmlNodePtr cur) {
     cur = cur->xmlChildrenNode;
     while (cur != NULL) {
 
-        /* Because we use pretty-formatted out XML documented with nice
-         * indentation, we have to use this test to ignore all the child nodes of
-         * <observation> that AREN'T elements--namely, all of the text nodes
-         * corresponding to the whitespace we introduced by pretty-formatting.  If
-         * we'd produced XML that looks like this:
+        /* Because we pretty-printed the XML document with nice indentation, we
+         * have to use this test to ignore all the child nodes of <observation>
+         * that AREN'T elements--namely, all of the text nodes corresponding to
+         * the whitespace we introduced by pretty-formatting.  If we'd produced
+         * XML that looks like this:
          *
          * <?xml version="1.0" encoding="UTF-8"?><data-set><observation year="2002" day-of-year="1" hour="0"><solar>0.000000</solar><temp units="°C">-13.050000</temp><rh>0.802061</rh><wind>3.118104</wind><precipitation>0.000000</precipitation></observation></data-set>,
          *
          * we wouldn't need this test. */
         if (cur->type == XML_ELEMENT_NODE) {
-            
+
             /* Note that it doesn't matter what order the parameter variables
              * are in.  On the other hand, there is no check in the code for
              * duplicate or missing variables.  We do check for unexpected
              * elements, however. */
-            if (xmlStrEqual(cur->name, "solar")) {
-                solar = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
-                printf("\tsolar: %s\n", solar);
-                xmlFree(solar);
-            }
-            else if (xmlStrEqual(cur->name, "temp")) {
-                temp = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
+            variable_name = cur->name;
+            if (recognized_variable(variable_name)) {
+                variable_value = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
                 units = xmlGetProp(cur, "units");
-                printf("\ttemp: %s%s\n", temp, units);
-                xmlFree(temp);
-            }
-            else if (xmlStrEqual(cur->name, "rh")) {
-                rh = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
-                printf("\trh: %s\n", rh);
-                xmlFree(rh);
-            }
-            else if (xmlStrEqual(cur->name, "wind")) {
-                wind = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
-                printf("\twind: %s\n", wind);
-                xmlFree(wind);
-            }
-            else if (xmlStrEqual(cur->name, "precipitation")) {
-                precipitation = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
-                printf("\tprecipitation: %s\n", precipitation);
-                xmlFree(precipitation);
+                if (units == NULL) {
+                    units = "";
+                }
+                printf("\t%s: %s%s\n", variable_name, variable_value, units);
+                xmlFree(variable_value);
             }
             else {
                 printf("UNEXPECTED ELEMENT!");
